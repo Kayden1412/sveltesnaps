3e22a720d3a9a348ad473257e8e8f917866d09af
Kayden1412 <143221653+Kayden1412@users.noreply.github.com>
2023-11-23 17:31:58 +0700

    Create gtag.svelte

---
+++ src/gtag.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { browser } from '$app/env';
+    export let id;
+
+    if (browser) {
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = function gtag(): void {
+            window.dataLayer!.push(arguments);
+        };
+        window.gtag('js', new Date());
+        window.gtag('config', id);
+    }
+</script>
+
+<svelte:head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id={id}"></script>
+</svelte:head>
